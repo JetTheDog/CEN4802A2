e9e1b8f7c60172d5bd0613fe6bbd8c815d818d2f
JetTheDog <79424948+JetTheDog@users.noreply.github.com>
2022-08-31 16:36:41 -0400

    Added javadocs for fib class

--- Main.java
+++ Main.java
@@ -3,6 +3,18 @@ public class Main {
         int num = 10;
         System.out.println("The " + num + "th number in the Fibonacci sequence is: " + fib(num));
     }
+
+    /**
+     * Returns the nth number in the Fibonacci sequence.
+     *
+     * @author          Jordan Bender
+     * @param  userNum  The numerical position that the user wants to find in the
+     *                  Fibonacci sequence (i.e. value of 5 = 5th number in sequence).
+     *
+     * @return          Returns userNum for positions less than or equal to 1,
+     *                  as their value in the sequence will be identical.
+     *                  Otherwise, iterates through algorithm.
+     */
     public static int fib(int userNum) {
         if (userNum <= 1)
             return userNum;
